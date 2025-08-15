61ed785a3e9798879118fe5e64dbae410dd4cf11
Atiye <Atiye@Z-LAPTOP>
2025-08-15 18:04:25 +0330

    Add project files.

---
+++ Resume.Presentation/Controllers/AdminController.cs
@@ -0,0 +1,17 @@
+﻿#region Using
+using Microsoft.AspNetCore.Mvc;
+
+namespace Resume.Presentation.Controllers;
+
+#endregion
+
+public class AdminController : Controller
+{
+    #region Admin
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    #endregion
+}
---
+++ Resume.Presentation/Models/Entities/Exprience/Exprience.cs
@@ -0,0 +1,11 @@
+﻿namespace Resume.Presentation.Models.Entities.Exprience;
+
+public class Exprience
+{
+    public int Id { get; set; }
+    public string ExprienceTitle { get; set; }
+    public string ExprienceDuration { get; set; }
+    public string Description { get; set; }
+    public string CompanyName { get; set; }
+    public string? CompanySite { get; set; }
+}
