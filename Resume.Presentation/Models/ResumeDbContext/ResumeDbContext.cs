2d0aae3a9bc73c7838838e4b3e572463a93f01af
Atiye <Atiye@Z-LAPTOP>
2025-11-02 20:26:11 +0330

    initial Education,exprience,my skills Models

---
+++ Resume.Presentation/Models/ResumeDbContext/ResumeDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Resume.Presentation.Models.Entities.Exprience;
+
+namespace Resume.Presentation.Models.ResumeDbContext;
+
+public class ResumeDbContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=Z-LAPTOP;Database=ResumeDb;User Id=sa;Password=123456789;TrustServerCertificate=True;");
+            base.OnConfiguring(optionsBuilder);
+    }
+    public DbSet<Exprience> Expriences{ get; set; }
+    public DbSet<Entities.Education.Education> Educations { get; set; }
+    public DbSet<Entities.MySkills.MySkills> MySkills { get; set; }
+}   
